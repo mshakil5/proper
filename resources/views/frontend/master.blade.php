957628eddad7c4ee3a2d3ab686efbff08bf1b095
Sakib <nsakib.miraz@gmail.com>
2026-02-01 23:31:35 +0600

    ..

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -115,7 +115,7 @@
         </script>
     @endif
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
     <link rel="icon" href="{{ asset('uploads/company/' . $company->fav_icon) }}" sizes="48x48">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet" />
