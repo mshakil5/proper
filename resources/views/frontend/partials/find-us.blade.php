731c9841553d2bd80006880e1e261f75ff8047b9
Sakib <nsakib.miraz@gmail.com>
2026-02-21 19:27:54 +0600

    ,,

--- resources/views/frontend/partials/find-us.blade.php
+++ resources/views/frontend/partials/find-us.blade.php
@@ -35,9 +35,12 @@
                 <div>
                     <h5 class="fw-bold">Opening Hours</h5>
                     <p class="company-info mb-2">
+                        Monday - Sunday: 4:00 PM - 10:00 PM
+                    </p>
+                    {{-- <p class="company-info mb-2">
                         Mon - Sat: 4:30pm - 11:30pm<br>
                         Sunday: 4:30pm - 10:00pm
-                    </p>
+                    </p> --}}
                     <small style="font-size:12px;" class="d-none">
                         <span style="color:red">*</span> Our online ordering website will stop taking orders ten minutes before we close.
                     </small>
