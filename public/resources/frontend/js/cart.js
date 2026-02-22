0cf7c24c1d6b77cde9d89e9bbfa55a97baa0875d
Sakib <nsakib.miraz@gmail.com>
2026-02-22 16:14:10 +0600

    ,,

--- public/resources/frontend/js/cart.js
+++ public/resources/frontend/js/cart.js
@@ -166,7 +166,8 @@ $(function () {
 
         let endTime = new Date(targetYear, targetMonth, targetDate, closeHour, closeMinute, 0);
 
-        while (current <= endTime) {
+        // while (current <= endTime) {
+        while ((current.getHours() * 60 + current.getMinutes()) + 20 <= closeHour * 60 + closeMinute) {
             let dayName = current.toLocaleDateString('en-GB', { weekday: 'long' });
             let start = current.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true });
             let end = new Date(current);
@@ -196,8 +197,8 @@ $(function () {
     }
 
     function updateDeliveryStartTimes() {
-        let deliverySlots = generateTimeSlots(16, 15);
-        let collectionSlots = generateTimeSlots(16, 0);
+        let deliverySlots = generateTimeSlots(16, 10);
+        let collectionSlots = generateTimeSlots(16, 10);
         
         if (deliverySlots.length > 0) {
             $('#deliveryStartTime').text(deliverySlots[0].label.split(' - ')[0]);
@@ -915,7 +916,7 @@ $(function () {
         if ($(this).val() === 'delivery') {
             $('#deliveryMode').show();
             $('#collectionMode').hide();
-            populateTimeSlots('#deliveryMode .delivery-time-select', 16, 15);
+            populateTimeSlots('#deliveryMode .delivery-time-select', 16, 10);
             $('#deliveryMode .delivery-time-select').val('');
             selectedDelivery.isValid = false;
             selectedDelivery.charge = 0;
@@ -923,7 +924,7 @@ $(function () {
         } else {
             $('#deliveryMode').hide();
             $('#collectionMode').show();
-            populateTimeSlots('#collectionMode .delivery-time-select', 16, 0);
+            populateTimeSlots('#collectionMode .delivery-time-select', 16, 10);
             $('#collectionMode .delivery-time-select').val('');
             selectedDelivery.isValid = true;
             selectedDelivery.charge = 0;
@@ -1117,7 +1118,7 @@ $(function () {
 
     loadDeliveryData();
     updateDeliveryStartTimes();
-    populateTimeSlots('#deliveryMode .delivery-time-select', 16, 15);
-    populateTimeSlots('#collectionMode .delivery-time-select', 16, 0);
+    populateTimeSlots('#deliveryMode .delivery-time-select', 16, 10);
+    populateTimeSlots('#collectionMode .delivery-time-select', 16, 10);
     updateCartUI();
 });
